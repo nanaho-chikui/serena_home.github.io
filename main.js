3be6dc25d6a8d895bbf626edf7156617a7409be9
Nanaho <nanaho_16n1100971@nnn.ed.jp>
2017-10-11 15:57:50 +0900

    再度うｐ

--- main.js
+++ main.js
@@ -1,24 +1,51 @@
-/*__________________________サイドバー____________________________________*/
+(function () {
+    'use strict';
 
-$(function() {
-    var duration = 500;
-    var $sidebar = $('.sidebar');
-    var $button = $('.button a');
 
-    $button.addClass('open');
-    $button.click(function() {
-        $button.toggleClass('close');
-        $button.removeClass('open');
-        if ($button.hasClass('close')) {
-            $sidebar.stop().animate({
-                left: '0'
-            }, duration, 'easeOutQuint');
-        } else {
-            $sidebar.stop().animate({
-                left: '-100px'
-            }, duration, 'easeOutQuint');
-            $button.addClass('open');
-        };
-    });
-});
+    const  nameInput= document.getElementById('name');
+    const  mailInput= document.getElementById('mail');
+    const  msgInput= document.getElementById('msg');
+    
 
+
+    const sendButton = document.getElementById('s_button');
+
+    
+// mailto リンクで外部メーラを起動しない
+user_pref("network.protocol-handler.external.mailto", false);
+
+sendButton.onclick = () => {
+    
+
+    console.log('Sendボタンを押しました');
+
+    const name = nameInput.value;
+    const mail = mailInput.value;
+    const msg = msgInput.value;
+
+    if (name.length === 0) { // 名前が空の時アラートを出し処理を終了する
+        console.log('名前を入力していません');
+
+  alert('名前を入力してください')
+    return;
+}
+
+if (mail.length === 0) { // メールアドレスが空の時アラートを出し処理を終了する
+    console.log('メールアドレスを入力していません');
+
+alert('メールアドレスを入力してください')
+return;
+}
+
+if (msg.length === 0) { // メッセージが空の時アラートを出し処理を終了する
+    console.log('メッセージを入力していません');
+
+alert('メッセージを入力してください')
+return;
+}
+
+
+
+}
+
+})();
